ac0087291c3a299db5c63395ef19734eb1bf69ce
Gusti Rahmat <gustirahmat@hotmail.com>
2019-05-23 14:46:25 +0700

    Force https and upload photos and videos
    Signed-off-by: Gusti Rahmat <gustirahmat@hotmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,19 +2,27 @@
 
 namespace harmoniKarimunJawa\Providers;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @param UrlGenerator $url
+	 *
+	 * @return void
+	 */
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+
+	    if (App::environment(['production', 'staging'])) {
+		    $url->forceScheme('https');
+	    }
     }
 
     /**
